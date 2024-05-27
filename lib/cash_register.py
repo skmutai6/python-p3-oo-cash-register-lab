e6bb683613cbb913c88bd1c67c0ad7ef40cf6dcb
Shadrack <skmutai6@gmail.com>
2024-05-27 23:10:13 +0300

    lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount = 0):
+        self.total = 0
+        self.discount = discount
+        self.items = []
+        self.transactions = []
+
+    def add_item(self, title, price, qty = 1):
+        self.total += price * qty
+        self.items.extend([title] * qty)
+        self.transactions.append((price, qty))
+
+    # def apply_discount(self):
+
+    # def void_last_transaction(self):
